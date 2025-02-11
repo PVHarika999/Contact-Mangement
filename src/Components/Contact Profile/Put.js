f8c176ed8d77ecaeaf4e352bcae2207e77733490
PVHarika999 <pvharika09@gmail.com>
2025-02-10 19:26:55 -0800

    Update Put.js

--- src/Components/Contact Profile/Put.js
+++ src/Components/Contact Profile/Put.js
@@ -1,1 +1,18 @@
+// PUT update contact by ID
+router.put('/:id', async (req, res) => {
+  const {error} = validateContact(req.body)
+  if (error) return res.status(400).send(error.details[0].message)
 
+  try {
+    const updatedContact = await Contact.findByIdAndUpdate(
+      req.params.id,
+      req.body,
+      {new: true},
+    )
+    if (!updatedContact)
+      return res.status(404).json({message: 'Contact not found'})
+    res.status(200).json(updatedContact)
+  } catch (err) {
+    res.status(500).json({message: 'Error updating contact'})
+  }
+})
