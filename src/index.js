b01457584a07473b967c9f46ca9d755d0e54a19f
PVHarika999 <pvharika09@gmail.com>
2025-02-10 18:50:35 -0800

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,1 +1,14 @@
+const mongoose = require('mongoose')
+
+const contactSchema = new mongoose.Schema({
+  name: {type: String, required: true},
+  phone: {type: String, required: true, unique: true},
+  email: {type: String, required: true, unique: true},
+  address: {type: String, optional: true},
+  createdAt: {type: Date, default: Date.now},
+})
+
+const Contact = mongoose.model('Contact', contactSchema)
+
+module.exports = Contact
 
